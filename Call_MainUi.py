987229f399d46fb8c7800713275fc47775209467
BruceJH_Lin <BruceJH_Lin@compal.com>
2023-04-18 10:28:54 +0800

    UI : Add form page for Funtionlist
    [Detailed Description]
    Add form page for Funtionlist
    
    [File type]
    UI

--- Call_MainUi.py
+++ Call_MainUi.py
@@ -14,7 +14,7 @@ class MainPageWindow(QtWidgets.QMainWindow,Ui_GBF_MAIN):
         self.setupUi(self)
         self.initUiindex()
         self.initbuttonUI()
-        self.change_group1()
+        self.change_Page1()#預設為page1
 
     def initUiindex(self):
         titleicon = QtGui.QIcon()
@@ -38,29 +38,43 @@ class MainPageWindow(QtWidgets.QMainWindow,Ui_GBF_MAIN):
 
     def initbuttonUI(self):
         self.actionHelp.triggered.connect(self.showDialog)
-        self.AddpushButton.clicked.connect(self.showDialog)
-        self.changebutton.clicked.connect(self.showDialog)
+        self.Funtionlist.clicked.connect(self.showDialog)
+        #self.AddpushButton.clicked.connect(self.showDialog)
+        #self.changebutton.clicked.connect(self.showDialog)
     
     def showDialog(self):
         sender = self.sender()
         if sender == self.actionHelp:
-            self.chooseSignal.emit('Help')
-        if sender == self.AddpushButton:
-            self.change_group1()
-        if sender == self.changebutton:
-            self.change_group2()
+            self.chooseSignal.emit('Help')        
+        if  sender == self.Funtionlist:
+            text = self.Funtionlist.currentItem().text()
+            if text == "轉世":
+                self.Info_broswer.setText("轉世")
+                self.change_Page1()
+            if text == "方陣":
+                self.Info_broswer.setText("方陣")
+                self.change_Page2()
+            if text == "方陣速刷":
+                self.Info_broswer.setText("方陣速刷")
+                self.change_Page3()
 
-    def change_group1(self):
+    def change_Page1(self):
         #horizontalLayout_7.replaceWidget(self.groupBox_2,self.groupBox_7,self.groupBox_16)
-        self.groupBox_2.setVisible(True) 
-        self.groupBox_7.setVisible(False)
-        self.groupBox_16.setVisible(False)
+        self.Page1.show() 
+        self.Page2.hide()
+        self.Page3.hide()
 
-    def change_group2(self):
+    def change_Page2(self):
         #horizontalLayout_7.replaceWidget(self.groupBox_2,self.groupBox_7,self.groupBox_16)
-        self.groupBox_2.setVisible(False)
-        self.groupBox_7.setVisible(True) 
-        self.groupBox_16.setVisible(False)
+        self.Page1.hide()
+        self.Page2.show() 
+        self.Page3.hide()
+
+    def change_Page3(self):
+        #horizontalLayout_7.replaceWidget(self.groupBox_2,self.groupBox_7,self.groupBox_16)
+        self.Page1.hide()
+        self.Page2.hide() 
+        self.Page3.show()
 
        
 
