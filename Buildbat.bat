28f973732f63688aea74486083c0c37cb0b84fc4
BruceJH_Lin <BruceJH_Lin@compal.com>
2023-06-29 17:35:32 +0800

    Code update

--- Buildbat.bat
+++ Buildbat.bat
@@ -2,6 +2,7 @@ cd UI\Homepage
 pyuic5.exe -o ui_MainUI.py .\MainUI.ui
 pyuic5.exe -o ui_Help.py .\Help.ui
 pyuic5.exe -o ui_Setting.py .\Setting.ui
+pyuic5.exe -o ui_BattleSetting.py .\BattleSetting.ui
 cd ..
 cd ..
 
